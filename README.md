223f11367c5b486eed68870990e9f06323cd2a24
lusanda <hlelalusa@gmail.com>
2025-08-01 19:25:41 +0200

    Task 0. API Development for Listings and Bookings in Django

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # ALX Travel App
 
-This Django project provides a platform for listing travel experiences. It includes models for Listings, Bookings, and Reviews. Seed data can be generated using a custom Django management command.
+## API Endpoints
+
+- GET /api/listings/
+- POST /api/listings/
+- GET /api/bookings/
+- POST /api/bookings/
+- Swagger UI: /swagger/
--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework.documentation import include_docs_urls
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -33,4 +34,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/', include('listings.urls')),
+    path('docs/', include_docs_urls(title='Travel App API')),
 ]
